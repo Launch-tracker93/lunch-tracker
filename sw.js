eea8f05f75414da697c18d34dfd683eee43e8b17
ff <chamiya210@gmail.com>
2026-03-26 15:15:21 +0900

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,24 @@
+const VERSION = '20260326-1';
+const CACHE = VERSION + '-cache';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
